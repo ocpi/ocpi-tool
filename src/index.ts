50ae9b94c235092034dc0d31ae4f28801a9e66f9
Reinier Lamers <reinier.lamers@ihomer.nl>
2022-11-18 15:09:04 +0100

    add basic command line parsing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hi");
+const { program } = require("commander");
+
+const loginTo = (url: string) => {
+  console.log("Aye, logging in, howdyhey");
+};
+
+const getAPage = (moduleName: string) => {
+  console.log("Aye, getting teh module");
+};
+
+program
+  .command("login <URL>")
+  .description("Log in to an OCPI platform")
+  .action((url: string) => loginTo(url));
+program
+  .command("get <module>")
+  .description("Fetch a page of data of a certain OCPI module")
+  .action((moduleName: string) => getAPage(moduleName));
+
+program.parse();
