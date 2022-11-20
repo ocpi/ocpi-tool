120a8d8516b0c16b1c35b2aa0521fd66000eb627
Reinier Lamers <reinier.lamers@ihomer.nl>
2022-11-20 14:43:57 +0100

    make sure import of generated type works

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
-const { program } = require("commander");
+import { Command } from "commander";
+import { V211Token } from "./ocpimsgs/token.schema";
 
-const loginTo = (url: string) => {
+const aargh: V211Token = {
+  uid: "AAAAAAAAAAAAAA",
+  type: "OTHER",
+};
+
+const use = (platformVersionsUrl: string) => {
   console.log("Aye, logging in, howdyhey");
 };
 
@@ -8,10 +14,12 @@ const getAPage = (moduleName: string) => {
   console.log("Aye, getting teh module");
 };
 
+const program = new Command();
+
 program
   .command("login <URL>")
   .description("Log in to an OCPI platform")
-  .action((url: string) => loginTo(url));
+  .action((url: string) => use(url));
 program
   .command("get <module>")
   .description("Fetch a page of data of a certain OCPI module")
