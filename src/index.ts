02a27ca4dcc9c99068fa7e193e7acd0a20518676
Reinier Lamers <reinier.lamers@ihomer.nl>
2022-11-25 13:27:17 +0100

    make login work when given version URL

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Command } from "commander";
 import { V211Version as V211ListedVersion } from "./ocpimsgs/version.schema";
 import { V211Version } from "./ocpimsgs/versionGetDetailResponse.schema";
+import { writeFile } from "node:fs/promises";
 
 interface OcpiResponse<T> {
   data: T;
@@ -9,6 +10,9 @@ interface OcpiResponse<T> {
   status_message?: string;
   timestamp: string;
 }
+
+const SESSION_FILE =
+  process.env.OCPI_SESSION_FILE ?? `${process.env.HOME}/.ocpi`;
 
 const use = async (platformVersionsUrl: string, token?: string) => {
   // OK, we're going to start a session with a certain platform. So we have to:
@@ -20,34 +24,36 @@ const use = async (platformVersionsUrl: string, token?: string) => {
     throw new Error('A token is required with "ocpi login"');
   }
 
-  console.log("Aye, logging in, howdyhey");
-  await axios
-    .get(platformVersionsUrl, {
+  let resp;
+  try {
+    resp = await axios.get(platformVersionsUrl, {
       headers: { Authorization: `Token ${token}` },
-    })
-    .then((resp) => {
-      const ocpiResponse = resp.data as OcpiResponse<any>;
-
-      console.log(`ocpiResponse: ${ocpiResponse}`);
-
-      if (Array.isArray(ocpiResponse.data)) {
-        const versions = ocpiResponse.data as V211ListedVersion[];
-        console.log(`It's a versions endpoint; versions is ${versions}`);
-      } else if (
-        typeof ocpiResponse.data == "object" &&
-        ocpiResponse.data.version
-      ) {
-        const version = ocpiResponse.data as V211Version;
-        console.log(`It's a version endpoint; version is ${version}`);
-      }
-    })
-    .catch((error) => {
-      if (error.response) {
-        throw new Error(
-          `Failed to make OCPI request to platform: HTTP status is [${error.response.status}]; body is [${error.response.data}]`
-        );
-      }
     });
+  } catch (error) {
+    const axiosError = error as AxiosError;
+    if (axiosError.isAxiosError) {
+      throw new Error(
+        `Failed to make OCPI request to platform: HTTP status is [${axiosError.response?.status}]; body is [${axiosError.response?.data}]`
+      );
+    } else throw error;
+  }
+
+  const ocpiResponse = resp.data as OcpiResponse<any>;
+
+  console.log(`ocpiResponse: ${ocpiResponse}`);
+
+  if (Array.isArray(ocpiResponse.data)) {
+    const versions = ocpiResponse.data as V211ListedVersion[];
+    console.log(`It's a versions endpoint; versions is ${versions}`);
+  } else if (
+    typeof ocpiResponse.data == "object" &&
+    ocpiResponse.data.version
+  ) {
+    const version = ocpiResponse.data as V211Version;
+    console.log(`It's a version endpoint; version is ${version}`);
+    await writeFile(SESSION_FILE, JSON.stringify(ocpiResponse.data));
+    console.log(`Logged in to ${platformVersionsUrl}`);
+  }
 };
 
 const getAPage = (moduleName: string) => {
